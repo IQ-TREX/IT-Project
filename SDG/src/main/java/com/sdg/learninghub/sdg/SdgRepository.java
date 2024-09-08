a568715fb6a7b4dde88c9301742681168df8f52b
Viv <weda@student.unimelb.edu.au>
2024-09-08 21:20:01 +1000

    Added functionality for the search to be able to search for the sdg based on its id number.

--- SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
+++ SDG/src/main/java/com/sdg/learninghub/sdg/SdgRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SdgRepository extends JpaRepository<Sdg, Long>{
-    @Query("SELECT s From Sdg s WHERE CONCAT(s.title, ' ', s.content, ' ') LIKE %?1%")
+    @Query("SELECT s From Sdg s WHERE CONCAT(s.id, '',s.title, ' ', s.content, ' ') LIKE %?1%")
     List<Sdg> findAll(String keyword);
 }
